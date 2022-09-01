52a0d65e1fe47f4cdaedcebb08e6b808c88b29c6
yangbingqiao <yangbingqiao@finogeeks.com>
2022-09-01 22:18:43 +0800

    opt：配合核心SDK优化，不再完全覆盖核心SDK的代理事件以及跨进程通信事件；
    (cherry picked from commit 8c9588e8c1688994b0a751936d7669b009a55a4f)

--- wechat/src/main/java/com/finogeeks/mop/wechat/WeChatMainProcessCallHandler.kt
+++ wechat/src/main/java/com/finogeeks/mop/wechat/WeChatMainProcessCallHandler.kt
@@ -4,15 +4,25 @@ import com.finogeeks.lib.applet.client.FinAppProcessClient
 import com.finogeeks.lib.applet.interfaces.FinCallback
 import com.finogeeks.lib.applet.sdk.api.IAppletProcessApiManager
 import com.google.gson.Gson
+import org.json.JSONObject
 
 internal class WeChatMainProcessCallHandler : IAppletProcessApiManager.MainProcessCallHandler {
 
     companion object {
+        const val CALL_NAME = "weChatSDK"
         const val API_NAME_GET_FIN_APP_INFO = "getFinAppInfo"
     }
 
     override fun onMainProcessCall(name: String, params: String?, callback: FinCallback<String>?) {
-        when (name) {
+        if (CALL_NAME != name) {
+            return
+        }
+        val apiName = try {
+            JSONObject(params ?: "{}").optString("apiName")
+        } catch (ignore: Exception) {
+            ""
+        }
+        when (apiName) {
             API_NAME_GET_FIN_APP_INFO -> getFinAppInfo(callback)
         }
     }
